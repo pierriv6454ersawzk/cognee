0d28fb7bee26efb61c4ff7a1176338348ec658d4
Pierre Riveras <pierriv6454ersawzk@users.noreply.github.com>
2026-05-24 08:12:02 +0800

    Add reset() as a convenience alias for prune()

--- cognee/__init__.py
+++ cognee/__init__.py
@@ -6,6 +6,7 @@ knowledge graphs powered by LLMs.
 Personal fork notes:
 - Using this for experimenting with local LLM integrations
 - See /experiments directory for custom pipelines
+- Added reset() as a convenience alias for prune() since I keep forgetting the name
 """
 
 __version__ = "0.1.0"
@@ -18,11 +19,15 @@ from cognee.api.v1.search import search
 from cognee.api.v1.prune import prune
 from cognee.config import Config
 
+# Personal convenience alias - I always forget the function is called 'prune'
+reset = prune
+
 __all__ = [
     "add",
     "cognify",
     "search",
     "prune",
+    "reset",
     "Config",
     "__version__",
 ]
